0be87a8c23700d24d00f8ffe7f8370e74b287394
Thomas <thomasbulle0@gmail.com>
2018-12-20 21:12:54 +0100

    Add page user

--- front/src/components/PageParticulier.jsx
+++ front/src/components/PageParticulier.jsx
@@ -1,3 +1,59 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import './PageParticulier.scss';
+import MesEquipements from './MesEquipements';
+import MesUsagesInternet from './MesUsagesInternet';
 
-class PageParticulier extends 
+
+class PageParticulier extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ongletActif: 'mesEquipements',
+      activeMesEquipements: 'active',
+      activeMesUsagesInternet: '',
+    }
+    this.handleClickNavPage = this.handleClickNavPage.bind(this);
+  }
+
+  handleClickNavPage(e) {
+    // eslint-disable-next-line default-case
+    switch(e.target.name) {
+      case 'mesEquipements':
+        this.setState({
+          ongletActif: e.target.name,
+          activeMesEquipements: 'active',
+          activeMesUsagesInternet: '',
+        });
+        break;
+      case 'mesUsagesInternet':
+        this.setState({
+          ongletActif: e.target.name,
+          activeMesEquipements: '',
+          activeMesUsagesInternet: 'active',
+        });
+        break;
+    };
+  }
+
+  render() {
+    return (
+      <div className="PageParticulier">
+        <h1 className="titlePagePerso">Particulier</h1>
+        <nav className="navPageParticulier">
+            <Button className={`btnNavPageParticulier ${this.state.activeMesEquipements}`} name="mesEquipements" onClick={this.handleClickNavPage}>
+              Mes équipements
+            </Button>
+            <Button className={`btnNavPageParticulier ${this.state.activeMesUsagesInternet}`} name="mesUsagesInternet" onClick={this.handleClickNavPage}>
+              Mes usages Internet
+            </Button>
+        </nav>
+        <div>
+          {(this.state.ongletActif === "mesEquipements") ? <MesEquipements /> : <MesUsagesInternet />}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PageParticulier;
