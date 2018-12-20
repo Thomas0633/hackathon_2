282899b34fd5f06cec86f6b55212185566717be6
Vivien POUGET <vivien.pouget@hotmail.fr>
2018-12-20 18:20:06 +0100

    add equipment

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AddEquipment from './components/AddEquipment';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AddEquipment/>
       </div>
     );
   }
