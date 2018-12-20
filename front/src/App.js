d790a1e109708db24cc5badbf0b63bd700eacfaf
Manon Buron <manon.buron@hotmail.fr>
2018-12-20 20:58:36 +0100

    footer

--- front/src/App.js
+++ front/src/App.js
@@ -5,8 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePageof from './components/HomePageof';
 import PageParticulier from './components/PageParticulier';
 import Contactform from './components/Contactform';
-import TemplateCards from './components/TemplateCards';
-import Footer from './components/Footer';
+
 
 
 
@@ -20,8 +19,6 @@ class App extends Component {
           <Route path="/contact" component={Contactform} />
         </Switch>
         <BarreNav />
-        <TemplateCards />
-        <Footer />
       </div>
     );
   }
