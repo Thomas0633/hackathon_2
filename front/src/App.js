c7bbc5f1def313b4606f380ca1fb9d0d074c603b
Julien Latappy <julienlatappy@gmail.com>
2018-12-20 23:52:29 +0100

    correction bug

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import BarreNav from "./components/BarreNav";
 import { Switch, Route } from "react-router-dom";
-import HomePageof from './components/HomePageof';
+import HomePage from './components/HomePage';
 import PageParticulier from './components/PageParticulier';
 import Contactform from './components/Contactform';
 import AddEquipment from './components/AddEquipment';
@@ -13,10 +13,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <div>
         <BarreNav />
+        </div>
         <Switch>
           <Route path="/add-equipment" component={AddEquipment} />
-          <Route exact path="/" component={HomePageof} />
+          <Route exact path="/" component={HomePage} />
           <Route path="/page-particulier" component={PageParticulier} />
           <Route path="/contact" component={Contactform} />
         </Switch>
