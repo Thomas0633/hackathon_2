822ba2bbb29afdeacb98bc817be14a70b69c9ef0
Vivien POUGET <vivien.pouget@hotmail.fr>
2018-12-20 18:48:17 +0100

    rearrange bugs

--- front/src/App.js
+++ front/src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import "./App.css";
 import BarreNav from "./components/BarreNav";
 import { Switch, Route } from "react-router-dom";
-import HomePageof from './components/HomePageof';
-import PageParticulier from './components/PageParticulier';
-import Contactform from './components/Contactform';
+// import HomePageof from './components/HomePageof';
+// import PageParticulier from './components/PageParticulier';
+// import Contactform from './components/Contactform';
 import AddEquipment from './components/AddEquipment';
 
 
@@ -12,13 +12,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={HomePageof} />
-          <Route path="/add-equipment" component={AddEquipment} />
-          <Route path="/page-particulier" component={PageParticulier} />
-          <Route path="/contact" component={Contactform} />
-        </Switch>
         <BarreNav />
+        <Switch>
+          <Route path="/add-equipment" component={AddEquipment} />
+        </Switch>
       </div>
     );
   }
