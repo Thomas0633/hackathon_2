7b3a70fdc27a5609cf5f7935ca04ef14df2dcfad
Jonathan Orillard <jonathan.orillard@hotmail.fr>
2018-12-21 11:27:33 +0100

    bilan V1

--- front/src/components/Bilan.jsx
+++ front/src/components/Bilan.jsx
@@ -1,30 +1,69 @@
 import React, { Component } from 'react';
 import './bilan.scss';
-import { Progress, Container } from 'reactstrap';
+import { Progress, Collapse, Button, CardBody, Card, Container } from 'reactstrap';
 
 class Bilan extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.toggle = this.toggle.bind(this);
+    this.state = { collapse: false };
   }
+
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+
   render() {
     return (
       <Container>
-        <hr/>
-        <h1>Votre bilan !</h1>
+        <a className="fancy-button bg-gradient1">
+                  <span onClick={this.toggle} style={{ marginBottom: '1rem' }}>
+                    Je veux voir mon bilan !
+                  </span>
+                </a>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
 
-        <h2>Bilan total</h2>
-        <div className="text-center">70%</div>
-        <Progress value={70} /><br />
+              <h2 className="pb-5">Détails de votre consommation :</h2>
 
-        <h2>Impact carbonne de vos équiments :</h2>
-        <div className="text-center">75%</div>
-        <Progress value={75} color="danger"/><br />
+              <h2>Navigation & Mails :</h2>
+              <div className="text-center">42%</div>
+              <Progress className="mb-5" value={42} color="success" /><br />
 
-        <h2>Impact carbonne de votre usage internet :</h2>
-        <div className="text-center">55%</div>
-        <Progress value={55} color="warning"/><br />
+              <h2>Smartphones & Tablettes :</h2>
+              <div className="text-center">71%</div>
+              <Progress  className="mb-5"value={71} color="danger" /><br />
 
+              <h2>Éco-Conception :</h2>
+              <div className="text-center">29%</div>
+              <Progress className="mb-5" value={29} color="success" /><br />
+
+              <h2>Smart Utilisation :</h2>
+              <div className="text-center">67%</div>
+              <Progress className="mb-5" value={67} color="warning" /><br />
+
+              <h2>Impact carbonne de vos équiments :</h2>
+              <div className="text-center">75%</div>
+              <Progress className="mb-5" value={75} color="danger" /><br />
+
+              <h2>Impact carbonne de votre usage internet :</h2>
+              <div className="text-center">55%</div>
+              <Progress className="mb-5" value={55} color="warning" /><br />
+
+
+              <h1>Score total :</h1>
+              <h1 className="mb-5 score">7/10</h1>
+
+              <h5 className="mb-3">PRECONISATIONS APRES AFFICHAGE DU BILAN :</h5>
+              <h5>-  N'appuyez pas automatiquement sur le bouton « Rechercher » de Google : vous émettez l’équivalent de 5 à 7 grammes de CO².</h5>
+              <h5>-  Réduisez vos envois de mails inutiles : envoyer un simple mail rejette 10 grammes de CO² (l’équivalent de ce que peut absorber un arbre en une journée).</h5>
+              <h5>-  Réduisez votre temps d’utilisation d’une heure d’Internet : c’est la consommation de 4000 tonnes de pétrole soit 4000 allers-retours Paris, New-York en avion !</h5>
+
+            </CardBody>
+          </Card>
+        </Collapse>
       </Container>
     );
   }
